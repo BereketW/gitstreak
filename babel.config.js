df76fe64bdffe44def45788f6d23188b1f231154
Bereket Wolde <bereket.wolde@astu.edu.et>
2026-03-26 21:59:57 +0300

    chore: update babel and metro config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
 module.exports = function (api) {
     api.cache(true);
+    const plugins = [];
+    if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
+        plugins.push('transform-remove-console');
+    }
     return {
         presets: [
             ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
             'nativewind/babel'
         ],
+        plugins,
     };
 };
